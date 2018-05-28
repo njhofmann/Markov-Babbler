f32cf305f72a31c5e36c45f4087ed54160deee45
NateJH <inatejh@gmail.com>
2018-05-28 19:17:35 -0400

    Added gitignore, added new save file system, cleaned up formatting

--- Markov-Babbler/mc_generator.py
+++ Markov-Babbler/mc_generator.py
@@ -1,29 +1,33 @@
-class Markov_Chain:
-    """From an input .txt file, generates a Markov Chain of all words in file"""
+class MarkovChain:
+    # From an input .txt file, generates a Markov Chain of all words in file"""
+
     def __init__(self, file_to_read):
         self.file_to_read = file_to_read
         self.converted_file = self.file_to_string()
         self.output_mc = self.create_markov_chain()
 
+    # Takes in a list, produces output list of each unique item in input list
     def unique_items(self, list):
-        #Takes in a list, produces ouput list of each unique item in input list
         items = []
         for i in list:
             if i not in items:
                 items.append(i)
         return items
 
+    # Takes the text in input file and converts it into one long string
     def file_to_string(self):
-        #Takes the text in input file and converts it into one long string
-        f = open(self.file_to_read)
+        try:
+            f = open(self.file_to_read)
+        except:
+            return ""
         string = f.read()
         return string
 
+    # Takes in file_to_string output, makes it a list where each word is a list,
+    # erases all unwanted characters like periods and colons, then lowers all letters
     def clean_input_text(self, text):
-        #Takes in file_to_string output, makes it a list where each word is a list,
-        #erases all unwanted characters like periods and colons, then lowers all letters
         text = text.split()
-        errors = [".", ",", "(", ")", ":", ";", "?", "!", "-"]
+        errors = [".", ",", "(", ")", ":", ";", "?", "!", "-", '"']
         for pos, word in enumerate(text):
             n = ""
             for l in word:
@@ -33,8 +37,8 @@ class Markov_Chain:
 
         return text
 
+    # Takes in output text from file_to_string, makes a list of all two words next to each other
     def generate_word_pairs(self, text):
-        #Takes in output text from file_to_string, makes a list of all two words next to each other
         word_pairs = []
         for pos in range(len(text)):
             if pos != (len(text) - 1):
@@ -43,39 +47,41 @@ class Markov_Chain:
                 word_pairs.append([text[pos], text[0]])
         return word_pairs
 
+    # Creates Markov Chain from
     def create_markov_chain(self):
-        #Creates Markov Chain from
         mc = {}
         cleaned_text = self.clean_input_text(self.converted_file)
         words = self.unique_items(cleaned_text)
         word_pairs = self.generate_word_pairs(cleaned_text)
         unique_wp = self.unique_items(word_pairs)
 
-        #Creates starting dictionary of every unique word in file
+        # Creates starting dictionary of every unique word in file
         for w in words:
             mc[w] = {}
 
-
         for w in mc:
-            t = 0 #How many times word appears in file
-            c = 0 #How many times iterated word appeared in previous word_pairs that had word as first element
+            t = 0  # How many times word appears in file
+            c = 0  # How many times iterated word appeared in previous word_pairs that had word as first element
             for pair in word_pairs:
-                if pair[0] == w: #If first word in iterated word pair matches iterated word, add 1 to t
+                if pair[0] == w:  # If first word in iterated word pair matches iterated word, add 1 to t
                     t += 1
 
-            for wp in unique_wp: #Iterates over each unique word_pair
-                if wp[0] == w: #If first word in unique word_pair matches currently iterated word
-                    a = 0 #How many times does unique word pair appear in file
+            for wp in unique_wp:  # Iterates over each unique word_pair
+                if wp[0] == w:  # If first word in unique word_pair matches currently iterated word
+                    a = 0  # How many times does unique word pair appear in file
                     l = []
-                    for pair in word_pairs: #Iterates over ALL word_pairs in file
-                        if wp == pair: #If iterated unique word_pair matches iterated word_pair in list of ALL word_pairs
+                    for pair in word_pairs:  # Iterates over ALL word_pairs in file
+                        if wp == pair:  # If iterated unique word_pair matches iterated word_pair in list of ALL
+                                        # word_pairs
                             a += 1
 
-                    for num in range(c, a + c): #Creates range from how many times word appear before to c + how many times
-                                                #word appeared in current iterated word_pair
+                    for num in range(c, a + c):  # Creates range from how many times word appear before to c + how many
+                                                 # times word appeared in current iterated word_pair
                         l.append(num)
                     c += a
-                    mc[w][wp[1]] = l #Adds unique word_pair and its generated range into Markov Chain for currently iterated word
-            mc[w] = [t, mc[w]] #Replaces dictionary value of each word with list
+                    mc[w][wp[
+                        1]] = l  # Adds unique word_pair and its generated range into Markov Chain for currently
+                                 # word currently being iterated over
+            mc[w] = [t, mc[w]]  # Replaces dictionary value of each word with list
 
         return mc
--- Markov-Babbler/window.py
+++ Markov-Babbler/window.py
@@ -1,65 +1,75 @@
 from tkinter import filedialog
 import tkinter as tk
 
-from mc_generator import Markov_Chain
+from mc_generator import MarkovChain
 from babbler import Markov_Babbler
 
+
 class Window:
-    """Creates a GUI to use for generating Markov Chains and Babbler text from a .txt file"""
+    # Creates a GUI to use for generating Markov Chains and Babbler text from a .txt file"""
+
     def __init__(self, master):
         self.master = master
         master.title("Markov Babbler")
 
-        #Constants
+        # Constants
         self.button_height = 3
         self.button_width = 10
         self.button_off_color = "Light Grey"
         self.button_on_color = "Grey"
 
-        #Displays babbler output
-        self.display = tk.Label(master, height=18, width=80, textvariable="", bg="White", borderwidth=3, relief="groove")
+        # Displays babbler output
+        self.display = tk.Label(master, height=18, width=80, textvariable="", bg="White", borderwidth=3,
+                                relief="groove")
         self.display.grid(row=0, rowspan=10, column=1, columnspan=10)
 
-        #Selects .txt file to make a babbler out of
+        # Selects .txt file to make a babbler out of
         self.select_file = tk.Button(master, text="Select File", height=self.button_height, width=self.button_width,
-                                     bg=self.button_off_color, activebackground=self.button_on_color, command=self.find_file)
+                                     bg=self.button_off_color, activebackground=self.button_on_color,
+                                     command=self.find_file)
         self.select_file.grid(row=0, column=0)
 
-        #Generates babbler text
+        # Generates babbler text
         self.generate = tk.Button(master, text="Generate Text", height=self.button_height, width=self.button_width,
-                                  bg=self.button_off_color, activebackground=self.button_on_color, command=self.generate_babble_text)
+                                  bg=self.button_off_color, activebackground=self.button_on_color,
+                                  command=self.generate_babble_text)
         self.generate.grid(row=1, column=0)
 
-        #Saves babbbler text in a .txt file
+        # Saves babbbler text in a .txt file
         self.save_file = tk.Button(master, text="Save Text", height=self.button_height, width=self.button_width,
-                                   bg=self.button_off_color, activebackground=self.button_on_color, command=self.save_babble)
+                                   bg=self.button_off_color, activebackground=self.button_on_color,
+                                   command=self.save_babble)
         self.save_file.grid(row=2, column=0)
 
-        #Placeholders for selected file's markov chain and babbler output
+        # Placeholders for selected file's markov chain and babbler output
         self.markov_chain = {}
         self.babble_output = ""
 
+    # Find the .txt file the user is wants to read from
     def find_file(self):
-        #Find the .txt file to use
-        selected_file = tk.filedialog.askopenfilename(initialdir="/",title="Select File",
-                                                      filetypes=(("txt Files","*.txt"),("All Files","*.*")))
-        mc = Markov_Chain(selected_file)
+        selected_file = tk.filedialog.askopenfilename(initialdir="/", title="Select File",
+                                                      filetypes=(("txt Files", "*.txt"), ("All Files", "*.*")))
+        mc = MarkovChain(selected_file)
         self.markov_chain = mc.output_mc
 
+    # From the file's generated markokv chain, produces 10 lines of babbler text
     def generate_babble_text(self):
-        #From the file's generated markokv chain, produces 10 lines of babbler text
         self.babble_output = ""
         for n in range(10):
             bab = Markov_Babbler(self.markov_chain)
             self.babble_output += bab.sentence + "\n"
         self.display.config(text=self.babble_output)
 
+    # Saves the last generated babble text as a .txt file under a file name and directory of user's choice
     def save_babble(self):
-        #Save generated babble text as a .txt file under "Markov_Babbler_Output.txt" in same file as program
-        save_file = open("Markov_Babbler_Ouput.txt", "w+")
+        save_file = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+        if save_file is None:
+            return
         save_file.write(self.babble_output)
         save_file.close()
 
-root = tk.Tk()
-my_window = Window(root)
-root.mainloop()
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    my_window = Window(root)
+    root.mainloop()
